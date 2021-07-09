9d6826a57b35c99084bcbd286ba969725b5dc784
shivamjjha <shivamjjha@gmail.com>
2021-07-10 00:03:20 +0530

    demo about, nested events page

---
+++ frontend/pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, at.</p>
+      <p>Version 1.0.0</p>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/pages/events/[slug].js
@@ -0,0 +1,9 @@
+const Event = () => {
+  return (
+    <div>
+      <h1>My Event</h1>
+    </div>
+  )
+}
+
+export default Event
