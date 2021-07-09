48db7aa1bce099a2ca08f9ee58a8159e9490ccd4
shivamjjha <shivamjjha@gmail.com>
2021-07-10 00:49:30 +0530

    added Header component with styling, included it in Layout

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import styles from '../styles/Layout.module.css'
+import Header from './Header';
+import styles from '../styles/Layout.module.css';
 
 const Layout = ({
   title = 'DJ Events | Find the hottest parties.',
@@ -14,6 +15,7 @@ const Layout = ({
         <meta name='desc' content={description} />
         <meta name='keywords' content={keywords} />
       </Head>
+      <Header></Header>
       <div className={styles.container}>{children}</div>
     </>
   );
