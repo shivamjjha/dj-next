b1de5a86fef7d56d5a8ad198861f7a721acc39d3
shivamjjha <shivamjjha@gmail.com>
2021-07-10 01:02:09 +0530

    exclamation icon on 404 page

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -1,17 +1,20 @@
-import Link from "next/link";
-import Layout from "../components/Layout"
+import Link from 'next/link';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import Layout from '../components/Layout';
 import styles from '../styles/404.module.css';
 
 const NotFound = () => {
   return (
     <Layout title='Page Not Found'>
       <div className={styles.error}>
-        <h1>404</h1>
+        <h1>
+          <FaExclamationTriangle /> 404
+        </h1>
         <h4>Sorry, there is nothing here</h4>
         <Link href='/'>Go back to home</Link>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
