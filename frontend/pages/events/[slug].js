2a9a35517cfe75ec4cdbd0678f7724f51b457e2c
shivamjjha <shivamjjha@gmail.com>
2021-07-10 00:13:43 +0530

    router object demons

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -1,9 +1,15 @@
+import { useRouter } from 'next/router';
+
 const Event = () => {
+  const router = useRouter();
+  console.log(router);
   return (
     <div>
       <h1>My Event</h1>
+      {router.query.slug}
+      <button onClick={() => router.push('/')}>Click</button>
     </div>
-  )
-}
+  );
+};
 
-export default Event
+export default Event;
