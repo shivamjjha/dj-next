fdbdbfe8be227fa05ac60e06ffaf239159d9dd5a
shivamjjha <shivamjjha@gmail.com>
2021-07-10 00:17:16 +0530

    Head Component demons

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,13 @@
+import Head from 'next/head'
+
 const HomePage = () => {
   return (
     <div>
+      <Head>
+        <title>DJ Events</title>
+        <meta name='description' content='Welcome to DJ Events'/>
+      </Head>
+
       <h1>Home</h1>
     </div>
   );
