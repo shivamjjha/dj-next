83b623d85a3fbc515e3211e697d3aa3c58f92bb8
shivamjjha <shivamjjha@gmail.com>
2021-07-10 19:53:44 +0530

    added EventItem component to home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
 // with aliases explicitly need to say `/index`
 import { API_URL } from '@/config/index';
 
@@ -8,6 +9,11 @@ const HomePage = ({ events }) => {
     <>
       <Layout>
         <h1>Upcoming Events</h1>
+        {events && events.length > 0 ? (
+          events.map(evt => <EventItem key={evt.id} evt={evt} />)
+        ) : (
+          <h3>No events to show</h3>
+        )}
       </Layout>
     </>
   );
