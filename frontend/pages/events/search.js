b69bbfb3e224fe899a4ca3085d131a3388235227
shivamjjha <shivamjjha@gmail.com>
2021-07-11 01:30:40 +0530

    search functionality implemented using qs

---
+++ frontend/pages/events/search.js
@@ -0,0 +1,48 @@
+import qs from 'qs';
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+// with aliases explicitly need to say `/index`
+import { API_URL } from '@/config/index';
+
+const SearchPage = ({ events }) => {
+  // console.log(events);
+  return (
+    <>
+      <Layout>
+        <h1>Events</h1>
+        {events && events.length > 0 ? (
+          events.map(evt => <EventItem key={evt.id} evt={evt} />)
+        ) : (
+          <h3>No events to show</h3>
+        )}
+      </Layout>
+    </>
+  );
+};
+
+export default SearchPage;
+
+/*
+ * We never know what someone's gonna search for,
+ * So we can't just generate static paths
+ */
+export async function getServerSideProps({ query: { term } }) {
+  // returns query string thta will be used to call strapy API
+  const query = qs.stringify({
+    _where: {
+      _or: [
+        { name_contains: term },
+        { performers_contains: term },
+        { description_contains: term },
+        { venue_contains: term },
+      ],
+    },
+  });
+
+  const res = await fetch(`${API_URL}/events?${query}`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
+}
