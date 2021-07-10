230efaf7516d10d3bd12169dcf36697847bfacd1
shivamjjha <shivamjjha@gmail.com>
2021-07-10 20:05:25 +0530

    individual event page

--- frontend/pages/events/index.js
+++ frontend/pages/events/index.js
@@ -1,11 +1,32 @@
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+// with aliases explicitly need to say `/index`
+import { API_URL } from '@/config/index';
 
-const EventsPage = () => {
+const Events = ({ events }) => {
+  console.log(events);
   return (
-    <Layout>
-      <h1>My Events</h1>
-    </Layout>
-  )
-}
+    <>
+      <Layout>
+        <h1>Events</h1>
+        {events && events.length > 0 ? (
+          events.map(evt => <EventItem key={evt.id} evt={evt} />)
+        ) : (
+          <h3>No events to show</h3>
+        )}
+      </Layout>
+    </>
+  );
+};
 
-export default EventsPage
+export default Events;
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
+}
