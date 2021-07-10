a30da761246f9250ddf47f05131dbe49e4289aa3
shivamjjha <shivamjjha@gmail.com>
2021-07-10 19:36:12 +0530

    getServerSideProps that will fetch data everytime on page load

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,26 @@
 import Layout from '@/components/Layout';
+// with aliases explicitly need to say `/index`
+import { API_URL } from '@/config/index';
 
-const HomePage = () => {
+const HomePage = ({ events }) => {
+  console.log(events);
   return (
     <>
       <Layout>
-        <h1>Home</h1>
+        <h1>Upcoming Events</h1>
       </Layout>
     </>
   );
 };
 
 export default HomePage;
+
+// `getServerSideProps` Runs server side (everytime the page is loaded)
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
+}
