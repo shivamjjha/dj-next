bcc1a947332d9e88e87e3a595c0100ceb4497bcd
shivamjjha <shivamjjha@gmail.com>
2021-07-10 19:08:55 +0530

    sample events route with restricted method to only `GET`

--- frontend/pages/api/events/index.js
+++ frontend/pages/api/events/index.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { events } = require('./data.json');
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'GET') {
+    res.status(200).json(events);
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).json({ message: `${req.method} is not allowed` });
+  }
 }
