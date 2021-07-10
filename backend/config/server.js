3d395a85d07f25b625dd0eae907a4b5c0b838f7c
shivamjjha <shivamjjha@gmail.com>
2021-07-10 22:06:09 +0530

    added strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7c932c16695a0801a4d569654edea131'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '586c786b-8bb4-43df-8689-b5891175b5ec'
+};
