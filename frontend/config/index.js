291138d56a3163a77580a689f67ec92881a085f9
shivamjjha <shivamjjha@gmail.com>
2021-07-11 22:47:24 +0530

    added Front end api next_url in config as per dev or prod

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,5 +1,9 @@
-// get base URL on prod or dev env
+// get API URL on prod or dev env
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
+
+// get NEXT FRONTEND URL on prod or dev env
+export const NEXT_URL =
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
 
 export const PER_PAGE = 5;
