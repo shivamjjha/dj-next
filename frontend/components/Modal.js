1efb11b8dc2390329b17e18fc93c211942ae1e5e
shivamjjha <shivamjjha@gmail.com>
2021-07-11 14:00:03 +0530

    Modal component with styles

---
+++ frontend/components/Modal.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import styles from '@/styles/Modal.module.css';
+import { FaTimes } from 'react-icons/fa';
+
+const Modal = ({ show, onClose, children, title }) => {
+  const [isBroswer, setIsBroswer] = useState(false);
+
+  useEffect(() => {
+    // On page render
+    setIsBroswer(true);
+  }, []);
+
+  const handleClose = () => {};
+
+  const modalContent = show && (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <div className={styles.header}>
+          <a href='#' onClick={handleClose}>
+            <FaTimes />
+          </a>
+        </div>
+        {title && <div>{title}</div>}
+        <div className={styles.body}>{children}</div>
+      </div>
+    </div>
+  );
+
+  // page is rendered on frontEnd, `window` obj is available
+  if (isBroswer) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.querySelector('#modal-root')
+    );
+  }
+};
+
+export default Modal;
