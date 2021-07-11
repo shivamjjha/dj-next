4aa23eb7d5debb3a87c954d0c600524b6245aa9e
shivamjjha <shivamjjha@gmail.com>
2021-07-12 00:04:43 +0530

    getting cookie in ServerSidePropsin req.header.cookie

--- frontend/pages/events/edit/[id].js
+++ frontend/pages/events/edit/[id].js
@@ -188,9 +188,11 @@ const EditEventPage = ({ evt }) => {
   );
 };
 
-export async function getServerSideProps({ params: { id } }) {
+export async function getServerSideProps({ params: { id }, req }) {
   const res = await fetch(`${API_URL}/events/${id}`);
   const evt = await res.json();
+
+  console.log(req.headers.cookie);
 
   return {
     props: { evt },
