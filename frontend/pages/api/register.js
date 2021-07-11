f84525a07bf0da8e4ba162428a4d23191af287e2
shivamjjha <shivamjjha@gmail.com>
2021-07-12 01:05:35 +0530

    register added in next APIs

---
+++ frontend/pages/api/register.js
@@ -0,0 +1,51 @@
+import cookie from 'cookie';
+import { API_URL } from '@/config/index';
+
+const login = async (req, res) => {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).json({ message: `Method: ${req.method} not allowed` });
+    return;
+  }
+
+  const { username, password, email } = req.body;
+
+  const strpiRes = await fetch(`${API_URL}/auth/local/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      password,
+      email,
+    }),
+  });
+
+  const data = await strpiRes.json();
+  console.log(data);
+
+  if (!strpiRes.ok) {
+    res
+      .status(data.statusCode)
+      .json({ message: data.message[0].messages[0].message });
+
+    return;
+  }
+
+  // Set Http-Only cookie
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('token', data.jwt, {
+      httpOnly: 'true',
+      // Check if prod
+      secure: process.env.NODE_ENV !== 'development',
+      maxAge: 60 * 60 * 24 * 7, // 1 week
+      sameSite: 'strict',
+      path: '/',
+    })
+  );
+  res.status(200).json({ user: data.user });
+};
+
+export default login;
