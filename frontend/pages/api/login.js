29a80acdaaad9218b9fa2d505480fc45a778fd29
shivamjjha <shivamjjha@gmail.com>
2021-07-11 23:59:25 +0530

    set HttpOnly cookie

--- frontend/pages/api/login.js
+++ frontend/pages/api/login.js
@@ -1,3 +1,4 @@
+import cookie from 'cookie';
 import { API_URL } from '@/config/index';
 
 const login = async (req, res) => {
@@ -21,7 +22,6 @@ const login = async (req, res) => {
   });
 
   const data = await strpiRes.json();
-  console.log(data);
 
   if (!strpiRes.ok) {
     res
@@ -31,7 +31,18 @@ const login = async (req, res) => {
     return;
   }
 
-  // @todo Set Http-Only cookie
+  // Set Http-Only cookie
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('token', data.jwt, {
+      httpOnly: 'true',
+      // Check if prod
+      secure: process.env.NODE_ENV !== 'development',
+      maxAge: 60 * 60 * 24 * 7, // 1 week
+      sameSite: 'strict',
+      path: '/'
+    })
+  );
   res.status(200).json({ user: data.user });
 };
 
