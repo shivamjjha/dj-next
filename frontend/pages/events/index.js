c64accacd36b0b6ffc1f8a273527ec063c464674
shivamjjha <shivamjjha@gmail.com>
2021-07-11 19:42:44 +0530

    pagination URLs implemented

--- frontend/pages/events/index.js
+++ frontend/pages/events/index.js
@@ -32,8 +32,11 @@ const Events = ({ events }) => {
 export default Events;
 
 export async function getServerSideProps({ query: { page = 1 } }) {
+  // Calculate start page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
   const res = await fetch(
-    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}`
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
   );
   const events = await res.json();
 
