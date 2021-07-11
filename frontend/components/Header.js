26ebd835ca8e1f479e46fef0785969409a3024c6
shivamjjha <shivamjjha@gmail.com>
2021-07-11 21:23:53 +0530

    Header links hidden and shown according to whether user is logged in or not

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 import Search from './Search';
 import styles from '@/styles/Header.module.css';
-import { FaSignInAlt } from 'react-icons/fa';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { useContext } from 'react';
+import AuthContext from '@/context/authContext';
 
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -23,27 +27,35 @@ const Header = () => {
             </Link>
           </li>
 
-          <li>
-            <Link href='/events/add'>
-              <a>Add Event</a>
-            </Link>
-          </li>
-
-          <li>
-            <Link href='/account/login'>
-              <a className='btn-secondary btn-icon'>
-                <FaSignInAlt /> Login
-              </a>
-            </Link>
-          </li>
-
-          <li>
-            <Link href='/account/register'>
-              <a className='btn-secondary btn-icon'>
-               Register
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            <>
+              <li>
+                <Link href='/events/add'>
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <button className='btn-secondary btn-icon' onClick={logout}>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href='/account/login'>
+                  <a className='btn-secondary btn-icon'>
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/account/register'>
+                  <a className='btn-secondary btn-icon'>Register</a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
