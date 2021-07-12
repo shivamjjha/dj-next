9fdfd6e559fdcf9da3154b167f626e08bde25b6c
shivamjjha <shivamjjha@gmail.com>
2021-07-12 22:41:33 +0530

    getting events from our custom created endpoint using getServerSideProps in dashboard page

--- frontend/pages/account/dashboard.js
+++ frontend/pages/account/dashboard.js
@@ -1,7 +1,31 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import parseCookie from '@/helpers/index';
 
-const DashboardPage = () => {
-  return <Layout title='User Dashboard'></Layout>;
+const DashboardPage = ({ events }) => {
+  console.log(events);
+  return (
+    <Layout title='User Dashboard'>
+      <h1>Dashboard</h1>
+    </Layout>
+  );
 };
 
 export default DashboardPage;
+
+export const getServerSideProps = async ({ req }) => {
+  const { token } = parseCookie(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
+};
